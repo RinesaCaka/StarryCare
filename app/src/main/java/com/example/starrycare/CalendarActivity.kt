b0254f790531c1d17c13bc94138fde740a811ad7
SuhejlaHoxha <121345932+SuhejlaHoxha@users.noreply.github.com>
2025-03-26 14:47:59 +0100

    First Page Loader

--- app/src/main/java/com/example/starrycare/CalendarActivity.kt
+++ app/src/main/java/com/example/starrycare/CalendarActivity.kt
@@ -8,16 +8,26 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import java.util.Calendar
 
-class MainActivity : AppCompatActivity() {
+class CalendarActivity : AppCompatActivity() {
+    private lateinit var calendarView: CalendarView
+    private lateinit var calendar: Calendar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.calendar_view)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.calendarMain)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        calendarView = findViewById(R.id.calendarView)
+        calendar = Calendar.getInstance()
+
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            calendar.set(year, month, dayOfMonth)
         }
     }
 }
